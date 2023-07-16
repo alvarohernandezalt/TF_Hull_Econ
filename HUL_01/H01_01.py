27b24d69f9c3f5f555235ab106c108ce845ff216
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-16 12:40:59 +0200

    Updated H01_01.py

--- HUL_01/H01_01.py
+++ HUL_01/H01_01.py
@@ -8,4 +8,12 @@ Y = tf.constant([[2],[4]], tf.float32)
 
 # Matrix multiply X by X's transpose and invert
 beta_0 = tf.linalg.inv(tf.matmul(tf.transpose(X), X))
-print(beta_0)
+print(beta_0.numpy())
+
+# Matrix multiply beta_0 by X's transpose
+beta_1 = tf.matmul(beta_0, tf.transpose(X))
+print(beta_1.numpy())
+
+# Matrix multiply beta_1 by Y
+beta = tf.matmul(beta_1, Y)
+print(beta.numpy())
