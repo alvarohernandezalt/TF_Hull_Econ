1c674da821182536affa96bada0a07d9a92c8a0b
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-21 18:34:33 +0200

    Update H01_24.py

--- HUL_01/H01_24.py
+++ HUL_01/H01_24.py
@@ -1,3 +1,14 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 
+# Declare image folder and image name
+image_name='prior-1-1.jpg'
+image_dir='D:\zCODE\04_SD_Training\training\Prior'
+
+# open image to numpy array and switch to RGB from BGR
+img = cv2.imread(os.path.join(image_dir,image_name))
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Import image data using numpy
+images = np.load('images.npy')
